43395731f5098bab100e8b64b5419b92846450ed
William Zhen <william.zhen8166@hotmail.com>
2024-01-07 18:25:09 -0500

    mobile- view

--- web/src/app/(auth)/components/Sidebar.tsx
+++ web/src/app/(auth)/components/Sidebar.tsx
@@ -1,16 +1,23 @@
 import Logo from "@/components/elements/Logo";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 interface SidebarProps {
+  className?: string;
   imageSrc: string;
   text?: string;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ imageSrc, text }) => {
+const Sidebar: React.FC<SidebarProps> = ({ className, imageSrc, text }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-brink-pink-200 w-[720px]">
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center h-full bg-brink-pink-200 w-[580px]",
+        className
+      )}
+    >
       <Link href="/">
         <Logo className="absolute text-white h-10 top-10 left-16" />
       </Link>
@@ -23,7 +30,9 @@ const Sidebar: React.FC<SidebarProps> = ({ imageSrc, text }) => {
         className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
       />
       {text && (
-        <h2 className="h2-bold max-w-xl text-white text-center">{text}</h2>
+        <h2 className="text-4xl font-medium max-w-lg text-white text-center">
+          {text}
+        </h2>
       )}
     </div>
   );
--- web/src/app/(auth)/signup/page.tsx
+++ web/src/app/(auth)/signup/page.tsx
@@ -4,11 +4,12 @@ const Signup = () => {
   return (
     <>
       <Sidebar
+        className="hidden lg:flex"
         imageSrc="/signup.svg"
         text="Sign up to save your recipes, make meal plans, and more."
       />
-      <div className="flex flex-1 items-center">
-        <div className="ml-28 px-14 py-7 w-full max-w-md">
+      <div className="flex flex-1 items-center justify-center 2xl:justify-start">
+        <div className="px-14 py-7 w-full max-w-md 2xl:ml-28">
           <h2 className="text-2xl font-medium">Sign up to Recipebean</h2>
           <form className="flex flex-col">
             <input
